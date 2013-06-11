51445a71f2b4b0f6fd630343587a3a728988b24b
Andrew Kesterson <andrew@aklabs.net>
2013-06-11 10:29:10 -0400

    Midstream on some bugfixes for list handling (AGAIN)

--- dpath/path.py
+++ dpath/path.py
@@ -3,10 +3,51 @@ import re
 import fnmatch
 import shlex
 import sys
+import traceback
+
+def path_types(obj, path):
+    """
+    Given a list of path name elements, return anew list of [name, type] path components, given the reference object.
+    """
+    result = []
+    #for elem in path[:-1]:
+    cur = obj
+    for elem in path[:-1]:
+        if ((issubclass(cur.__class__, dict) and elem in cur)):
+            result.append([elem, cur[elem].__class__])
+            cur = cur[elem]
+        elif (issubclass(cur.__class__, (list, tuple)) and int(elem) < len(cur)):
+            elem = int(elem)
+            result.append([elem, cur[elem].__class__])
+            cur = cur[elem]
+        else:
+            result.append([elem, dict])
+    try:
+        try:
+            result.append([path[-1], cur[path[-1]].__class__])
+        except TypeError:
+            result.append([path[-1], cur[int(path[-1])].__class__])
+    except (KeyError, IndexError):
+        result.append([path[-1], path[-1].__class__])
+    print "result = {}".format(result)
+    return result
+
+def paths_only(path):
+    """
+    Return a list containing only the pathnames of the given path list, not the types.
+    """
+    l = []
+    for p in path:
+        l.append(p[0])
+    return l
 
 def validate(path, separator="/", regex=None):
+    """
+    Validate that all the keys in the given list of path components are valid, given that they do not contain the separator, and match any optional regex given.
+    """
     validated = []
-    for key in path:
+    for elem in path:
+        key = elem[0]
         strkey = str(key)
         if (separator and (separator in strkey)):
             raise dpath.exceptions.InvalidKeyName("{} at {} contains the separator {}"
@@ -36,24 +77,24 @@ def paths(obj, dirs=True, leaves=True, path=[], skip=False, separator="/"):
 
     """
     if isinstance(obj, dict):
-        for (i, v) in enumerate(obj):
-            if skip and v[0] == '+':
+        for (k, v) in obj.iteritems():
+            if issubclass(k.__class__, (basestring)) and skip and k[0] == '+':
                 continue
-            newpath = path + [v]
+            newpath = path + [[k, v.__class__]]
             validate(newpath, separator=separator)
             if dirs:
                 yield newpath
-            for child in paths(obj[v], dirs, leaves, newpath, skip):
+            for child in paths(v, dirs, leaves, newpath, skip):
                 yield child
     elif isinstance(obj, (list, tuple)):
         for (i, v) in enumerate(obj):
-            newpath = path + [i]
+            newpath = path + [[i, v.__class__]]
             if dirs:
                 yield newpath
             for child in paths(obj[i], dirs, leaves, newpath, skip):
                 yield child
     elif leaves:
-        yield path + [obj]
+        yield path + [[obj, obj.__class__]]
     elif not dirs:
         yield path
 
@@ -85,15 +126,9 @@ def match(path, glob):
             ss_glob = glob[:ss] + glob[ss + 1:]
 
     if path_len == len(ss_glob):
-        return all(map(fnmatch.fnmatch, map(str, path), map(str, ss_glob)))
+        return all(map(fnmatch.fnmatch, map(str, paths_only(path)), map(str, ss_glob)))
 
     return False
-
-def search(obj, glob, dirs=True, leaves=False):
-    """Search the object paths that match the glob."""
-    for path in paths(obj, dirs, leaves, skip=True):
-        if match(path, glob):
-            yield path
 
 def is_glob(string):
     return any([c in string for c in '*?[]!'])
@@ -113,33 +148,36 @@ def set(obj, path, value, create_missing=True, separator="/", filter=None):
     traversed = []
 
     def _presence_test_dict(obj, elem):
-        return (elem in obj)
+        return (elem[0] in obj)
 
     def _create_missing_dict(obj, elem):
-        obj[elem] = {}
+        obj[elem[0]] = elem[1]()
 
-    def _presence_test_list(obj, idx):
-        return (idx < len(obj))
+    def _presence_test_list(obj, elem):
+        return (int(str(elem[0])) < len(obj))
 
-    def _create_missing_list(obj, idx):
-        idx = int(str(elem))
+    def _create_missing_list(obj, elem):
+        idx = int(str(elem[0]))
         while (len(obj)-1) < idx:
             obj.append(None)
 
     def _accessor_dict(obj, elem):
-        return obj[elem]
+        return obj[elem[0]]
 
     def _accessor_list(obj, elem):
-        return obj[int(str(elem))]
+        return obj[int(str(elem[0]))]
 
     def _assigner_dict(obj, elem, value):
-        obj[elem] = value
+        obj[elem[0]] = value
 
     def _assigner_list(obj, elem, value):
-        obj[int(str(elem))] = value
+        obj[int(str(elem[0]))] = value
 
     elem = None
     for elem in path:
+        elem_value = elem[0]
+        elem_type = elem[1]
+
         tester = None
         creator = None
         accessor = None
@@ -150,7 +188,7 @@ def set(obj, path, value, create_missing=True, separator="/", filter=None):
             accessor = _accessor_dict
             assigner = _assigner_dict
         elif issubclass(obj.__class__, (list, tuple)):
-            if not str(elem).isdigit():
+            if not str(elem_value).isdigit():
                 raise TypeError("Can only create integer indexes in lists, "
                                 "not {}, in {}".format(type(obj),
                                                        separator.join(traversed)
@@ -162,7 +200,8 @@ def set(obj, path, value, create_missing=True, separator="/", filter=None):
             assigner = _assigner_list
         else:
             raise TypeError("Unable to path into elements of type {} "
-                            "at {}".format(type(elem), separator.join(traversed)))
+                            "at {}".format(obj, separator.join(traversed)))
+
         if (not tester(obj, elem)) and (create_missing):
             creator(obj, elem)
         elif (not tester(obj, elem)):
@@ -172,7 +211,7 @@ def set(obj, path, value, create_missing=True, separator="/", filter=None):
                     separator.join(traversed)
                     )
                 )
-        traversed.append(elem)
+        traversed.append(elem_value)
         if len(traversed) < len(path):
             obj = accessor(obj, elem)
 
@@ -181,7 +220,7 @@ def set(obj, path, value, create_missing=True, separator="/", filter=None):
     if (filter and filter(accessor(obj, elem))) or (not filter):
         assigner(obj, elem, value)
 
-def get(obj, path, view=False):
+def get(obj, path, view=False, filter=None):
     """Get the value of the given path.
 
     Arguments:
@@ -198,25 +237,44 @@ def get(obj, path, view=False):
     head = type(target)()
     tail = head
     up = None
-    for key in path:
+    print path
+    print "===="
+    for pair in path:
+        print pair
+        print up
+        print tail
+        print head
+        key = pair[0]
+        print key
         target = target[key]
         if view:
             if isinstance(tail, dict):
-                if target == None:
-                    tail[key] = None
+                if issubclass(pair[1], (list, dict)):
+                    tail[key] = pair[1]()
                 else:
-                    tail[key] = type(target)()
-            elif isinstance(tail, list):
-                if key >= len(tail):
-                    tail += [None] * (key - len(tail) + 1)
-                if target == None:
                     tail[key] = None
-                else:
-                    tail[key] = type(target)()
-            up = tail
-            tail = tail[key]
+                print "Assigning up to tail because tail was a dict, pathing into tail : {}".format(tail)
+                up = tail
+                tail = tail[key]
+            elif issubclass(tail.__class__, (list, tuple)):
+                if issubclass(pair[1], (list, tuple, dict)):
+                    print "Appending object of type {} to tail".format(pair[1])
+                    tail.append(pair[1]())
+                    up = tail[-1]
+        if not issubclass(target.__class__, (list, dict)):
+            if (filter and (not filter(target))):
+                raise dpath.exceptions.FilteredValue
+        print up
+        print tail
+        print head
+        print "----"
     if view:
-        up[path[-1]] = target
+        if issubclass(up.__class__, (list, tuple)):
+            print "Appending {} to {} because up is a list/tuple".format(target, up)
+            up.append(target)
+        else:
+            print "Setting {}[{}] = {} because up is a dict".format(up, key, target)
+            up[key] = target
         return head
     else:
         return target
--- dpath/util.py
+++ dpath/util.py
@@ -15,7 +15,8 @@ def new(obj, path, value, separator="/"):
     characters in it, they will become part of the resulting
     keys
     """
-    return dpath.path.set(obj, path.lstrip(separator).split(separator), value, create_missing=True)
+    pathobj = dpath.path.path_types(obj, path.lstrip(separator).split(separator))
+    return dpath.path.set(obj, pathobj, value, create_missing=True)
 
 def delete(obj, glob, separator="/", filter=None):
     """
@@ -26,8 +27,7 @@ def delete(obj, glob, separator="/", filter=None):
     """
     deleted = 0
     paths = []
-    print separator
-    for path in dpath.path.search(obj, glob.lstrip(separator).split(separator)):
+    for path in _inner_search(obj, glob.lstrip(separator).split(separator)):
         # These are yielded back, don't mess up the dict.
         paths.append(path)
 
@@ -37,13 +37,13 @@ def delete(obj, glob, separator="/", filter=None):
         for item in path:
             prev = cur
             try:
-                cur = cur[item]
+                cur = cur[item[0]]
             except AttributeError, e:
                 # This only happens when we delete X/Y and the next
                 # item in the paths is X/Y/Z
                 pass
-        if (not filter) or (filter and filter(prev[item])):
-            prev.pop(item)
+        if (not filter) or (filter and filter(prev[item[0]])):
+            prev.pop(item[0])
         deleted += 1
     if not deleted:
         raise dpath.exceptions.PathNotFound("Could not find {} to delete it".format(glob))
@@ -55,7 +55,7 @@ def set(obj, glob, value, separator="/", filter=None):
     to the given value. Returns the number of elements changed.
     """
     changed = 0
-    for path in dpath.path.search(obj, glob.lstrip(separator).split(separator)):
+    for path in _inner_search(obj, glob.lstrip(separator).split(separator)):
         changed += 1
         dpath.path.set(obj, path, value, create_missing=False, filter=filter)
     return changed
@@ -72,21 +72,32 @@ def search(obj, glob, yielded=False, separator="/", filter=None):
 
     def _search_view(obj, glob):
         view = {}
-        for path in dpath.path.search(obj, glob.lstrip(separator).split(separator), dirs=False):
-            val = dpath.path.get(obj, path, view=False)
-            if (not filter) or (filter(val)):
-                dpath.path.set(view, path, val, create_missing=True)
+        for path in _inner_search(obj, glob.lstrip(separator).split(separator), dirs=(filter == None)):
+            try:
+                val = dpath.path.get(obj, path, filter=filter, view=True)
+                merge(view, val)
+            except dpath.exceptions.FilteredValue:
+                pass
         return view
 
     def _search_yielded(obj, glob):
-        for path in dpath.path.search(obj, glob.lstrip(separator).split(separator)):
-            val = dpath.path.get(obj, path, view=False)
-            if (not filter) or (filter and filter(val)):
-                yield (separator.join(path), val)
+        for path in _inner_search(obj, glob.lstrip(separator).split(separator), dirs=False):
+            print path
+            try:
+                val = dpath.path.get(obj, path, view=False, filter=filter)
+                yield (separator.join(map(str, dpath.path.paths_only(path))), val)
+            except dpath.exceptions.FilteredValue:
+                pass
 
     if yielded:
         return _search_yielded(obj, glob)
     return _search_view(obj, glob)
+
+def _inner_search(obj, glob, dirs=True, leaves=False):
+    """Search the object paths that match the glob."""
+    for path in dpath.path.paths(obj, dirs, leaves, skip=True):
+        if dpath.path.match(path, glob):
+            yield path
 
 def merge(dst, src, separator="/", filter=None, flags=MERGE_ADDITIVE, _path=""):
     """Merge source into destination. Like dict.update() but performs
@@ -104,47 +115,46 @@ def merge(dst, src, separator="/", filter=None, flags=MERGE_ADDITIVE, _path=""):
           replaces the destination in this situation.
     """
 
+    if filter:
+        # Having merge do its own filtering is dumb, let search do the
+        # heavy lifting for us.
+        src = search(src, '**', filter=filter)
+        return merge(dst, src)
+
     def _check_typesafe(obj1, obj2, key, path):
-        if ( (flags & MERGE_TYPESAFE) and (type(obj1[key]) != type(obj2[key]))):
+        if not key in obj1:
+            return
+        elif ( (flags & MERGE_TYPESAFE == MERGE_TYPESAFE) and (type(obj1[key]) != type(obj2[key]))):
             raise TypeError("Cannot merge objects of type {} and {} at {}"
                             "".format(type(obj1[key]), type(obj2[key]), path))
-
-
-    def _filter_assign(obj, key, value):
-        if (not filter) or (filter and filter(value)):
-            obj[key] = value
+        elif ( (flags & MERGE_TYPESAFE != MERGE_TYPESAFE) and (type(obj1[key]) != type(obj2[key]))):
+            obj1.pop(key)
 
     if isinstance(src, dict):
         for (i, v) in enumerate(src):
             _check_typesafe(dst, src, v, separator.join([_path, str(v)]))
 
             if not v in dst:
-                if not isinstance(src[v], (dict, list)):
-                    _filter_assign(dst, v, src[v])
-                else:
-                    dst[v] = src[v]
+                dst[v] = src[v]
             else:
                 if not isinstance(src[v], (dict, list)):
-                    _filter_assign(dst, v, src[v])
+                    dst[v] = src[v]
                 else:
                     merge(dst[v], src[v], filter=filter, flags=flags,
                           _path=separator.join([_path, str(v)]), separator=separator)
     elif isinstance(src, list):
         for (i, v) in enumerate(src):
             _check_typesafe(dst, src, i, separator.join([_path, str(i)]))
-
             dsti = i
             if ( flags & MERGE_ADDITIVE):
-                dsti += len(src)
+                dsti = len(dst)
             if dsti >= len(dst):
-                dst += [None] * (dsti - len(dst) + 1)
+                dst += [None] * (dsti - (len(dst) - 1))
             if dst[dsti] == None:
-                _filter_assign(dst, dsti, src[i])
+                dst[dsti] = src[i]
             else:
                 if not isinstance(src[i], (dict, list)):
-                    _filter_assign(dst, dsti, src[i])
+                    dst[dsti] = src[i]
                 else:
                     merge(dst[i], src[i], filter=filter, flags=flags,
-                          _path=separator.join(_path, v), separator=separator)
-
-
+                          _path=separator.join([_path, str(i)]), separator=separator)
