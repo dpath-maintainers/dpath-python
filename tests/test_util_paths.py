a4e7cb6bb5d5c32a47708814efc4f6cbc4e68876
Andrew Kesterson <andrew@aklabs.net>
2015-02-04 06:38:50 -0800

    Fixes #30: Add missing test

---
+++ tests/test_util_paths.py
@@ -0,0 +1,9 @@
+import nose
+from nose.tools import raises
+import dpath.util
+
+def test_util_safe_path_list():
+    res = dpath.util.__safe_path__(["Ignore", "the/separator"], None)
+    assert(len(res) == 2)
+    assert(res[0] == "Ignore")
+    assert(res[1] == "the/separator")
