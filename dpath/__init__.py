8d3a36f49ab62820488edc089decfe3fb693f5e8
moomoohk <moomoohk2@gmail.com>
2022-11-24 00:38:16 +0200

    Add exports

---
+++ dpath/__init__.py
@@ -0,0 +1,16 @@
+from dpath.util import new, delete, set, get, values, search, merge
+
+__all__ = [
+    "new",
+    "delete",
+    "set",
+    "get",
+    "values",
+    "search",
+    "merge",
+    "exceptions",
+    "options",
+    "segments",
+    "types",
+    "version",
+]
