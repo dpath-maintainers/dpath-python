3df2d7ef4a4360778aa282b50454c180a65c7b4c
moomoohk <2220203+moomoohk@users.noreply.github.com>
2022-12-04 19:29:09 +0200

    Add some OOP tests

--- tests/test_oop.py
+++ tests/test_oop.py
@@ -1,7 +1,9 @@
+from copy import deepcopy
+
 from dpath import DDict
 
 
-def test_getitem():
+def test_oop_getitem():
     d = DDict({
         "a": 1,
         "b": [12, 23, 34],
@@ -12,7 +14,7 @@ def test_getitem():
     # assert d["b/-1"] == 34
 
 
-def test_setitem():
+def test_oop_setitem():
     d = DDict({
         "a": 1,
         "b": [12, 23, 34],
@@ -25,7 +27,7 @@ def test_setitem():
     assert d["c"] == [54, 43, 32, 21]
 
 
-def test_setitem_overwrite():
+def test_oop_setitem_overwrite():
     d = DDict({
         "a": 1,
         "b": [12, 23, 34],
@@ -33,3 +35,98 @@ def test_setitem_overwrite():
 
     d["b"] = "abc"
     assert d["b"] == "abc"
+
+
+def test_oop_contains():
+    d = DDict({
+        "a": 1,
+        "b": [12, 23, 34],
+        "c": {
+            "d": {
+                "e": [56, 67]
+            }
+        }
+    })
+
+    assert "a" in d
+    assert "b" in d
+    assert "b/0" in d
+    assert "c/d/e/1" in d
+
+
+def test_oop_len():
+    d = DDict({
+        "a": 1,
+        "b": [12, 23, 34],
+    })
+
+    assert len(d) == 5
+
+
+def test_oop_merge():
+    d = DDict({
+        "a": 1,
+        "b": [12, 23, 34],
+        "c": {
+            "d": {
+                "e": [56, 67]
+            }
+        }
+    })
+
+    expected_after = {
+        "a": 1,
+        "b": [12, 23, 34],
+        "c": {
+            "d": {
+                "e": [56, 67]
+            }
+        },
+        "f": [54],
+    }
+
+    before = deepcopy(d)
+
+    assert d | {"f": [54]} == expected_after
+
+    assert d == before
+
+    d |= {"f": [54]}
+
+    assert d != before
+    assert d == expected_after
+
+
+def test_oop_keys():
+    d = DDict({
+        "a": 1,
+        "b": [12, 23, 34],
+        "c": {
+            "d": {
+                "e": [56, 67]
+            }
+        }
+    })
+
+    assert not set(d.keys()).difference({
+        "a",
+        "b",
+        "c",
+        "b/0",
+        "b/1",
+        "b/2",
+        "c/d",
+        "c/d/e",
+        "c/d/e/0",
+        "c/d/e/1",
+    })
+
+
+def test_oop_values():
+    d = DDict({
+        "a": 1,
+        "b": [12, 23, 34],
+    })
+
+    assert list(d.values()) == [1, [12, 23, 34], 12, 23, 34]
+
