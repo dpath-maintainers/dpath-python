cff529f91680d3983fc36652f933e22485067889
moomoohk <2220203+moomoohk@users.noreply.github.com>
2022-12-05 22:06:03 +0200

    Add documentation and bump version

--- dpath/options.py
+++ dpath/options.py
@@ -1,2 +1,4 @@
 ALLOW_EMPTY_STRING_KEYS = False
 REPLACE_NONE_VALUES_IN_LISTS = False
+"""When creating a new subtree within a list, this option will cause dpath to treat None items as non-significant and 
+overwrite them. Default behavior will cause an exception to be thrown instead."""
