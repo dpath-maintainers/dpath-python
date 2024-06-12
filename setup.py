a93d415b7caa9bf6fce60c5daf21a0f880d45d8c
moomoohk <2220203+moomoohk@users.noreply.github.com>
2024-06-13 00:16:25 +0300

    Add 3.12 support

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ if __name__ == "__main__":
             'Programming Language :: Python :: 3.9',
             'Programming Language :: Python :: 3.10',
             'Programming Language :: Python :: 3.11',
+            'Programming Language :: Python :: 3.12',
             'Topic :: Software Development :: Libraries :: Python Modules',
             'Typing :: Typed',
         ],
